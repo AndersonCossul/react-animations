f53b2dfa84b271787f6646f218837fe8efc4d42a
Anderson Cossul <anderson_cossul@hotmail.com>
2018-02-21 23:28:02 -0300

    Defining different enter and exit time on transitions.

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,13 +3,16 @@ import Transition from 'react-transition-group/Transition'
 
 import './Modal.css'
 
-const modal = (props) => {
-  
+const animationTiming = {
+  enter: 400,
+  exit: 1000
+}
 
+const modal = (props) => {
   return (
     <Transition
       in={props.show}
-      timeout={300}
+      timeout={animationTiming}
       mountOnEnter
       unmountOnExit >
       {state => {
